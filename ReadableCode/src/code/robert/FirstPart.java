4114d05129938801f6bd14aa79bbb26a0a6ba41e
yujingzhou <yujingzhou2010@gmail.com>
2013-08-29 10:01:55 +0800

    init readable code

---
+++ ReadableCode/src/code/robert/FirstPart.java
@@ -0,0 +1,33 @@
+package code.robert;
+
+/**
+ * 关键思想：
+ * 代码应当易于理解
+ * 代码的写法应当使别人理解的时间最小化
+ * 
+ * 关键思想：
+ * 把信息装入名字中。
+ * 选择专业的词。
+ * 避免泛泛的名字（或者说要知道什么时候使用它）。
+ * 用具体的名字代替抽象的名字。
+ * 使用前缀和后缀来给名字附带更多的信息。
+ * 决定名字的长度。
+ * 利用名字的格式来表达含义。
+ * 
+ * 关键思想：
+ * 清晰和准确比装可爱好。
+ * 
+ * @author robert
+ *
+ */
+public class FirstPart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
